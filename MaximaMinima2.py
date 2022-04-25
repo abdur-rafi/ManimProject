508f88ced6d05458e021a7b03e9e3a4f831217de
abdur-rafi <abdurrafi403@gmail.com>
2022-04-25 14:02:29 +0600

    readme updated

--- MaximaMinima2.py
+++ MaximaMinima2.py
@@ -24,10 +24,7 @@ class Maxima(ZoomedScene):
         )
 
     
-    # def incrDecr(self):
-
-
-    def construct(self):
+    def incrDecr(self):
         # tText = Text("Function", font_size=40).set_color_by_gradient(GREEN, BLUE)
         # tText.move_to(3.2 * UP)
 
@@ -225,8 +222,8 @@ class Maxima(ZoomedScene):
 
         def getSlope(func):
             x1 = x.get_value()
-            p1 = axes.c2p(x1, func(x1), 0)
-            p2 = axes.c2p(x1 + Dx, func(x1 + Dx), 0)
+            p1 = [x1, func(x1), 0]
+            p2 = [x1 + Dx, func(x1 + Dx), 0]
             slope = (p2[1] - p1[1]) / Dx
             return [x1, slope, 0]
         
@@ -265,5 +262,176 @@ class Maxima(ZoomedScene):
         negLine = Line(axes.c2p(0, 0, 0), lines[0].get_start() ).set_stroke(color = ORANGE, width = 2 * curveStrokeWidth)
         self.play(Create(negLine))
         self.wait(1)
+        # self.play(FadeOut(*self.mobjects))
         # axes.x_axis.set_
         # self.play(Uncreate(curve))
+
+    def turningPoint(self):
+        
+        sW = DEFAULT_STROKE_WIDTH / 1.5
+
+        axis_config = {
+            "tick_size" : .05,
+            "include_numbers" : False,
+
+        }
+        axes = Axes(
+            x_range=[-4.5 * PI, 4.5 * PI, PI],
+            y_range =[-3, 3, 1],
+            x_axis_config=axis_config,
+            y_axis_config=axis_config,
+            y_length=config.frame_height - .5
+
+        ).set_color_by_gradient(GREEN,RED, BLUE).scale(.9)
+        self.play(Create(axes))
+        def cosCurveFunc(x):
+            return np.cos(x)
+        cosCurve = axes.plot(cosCurveFunc, x_range= [-4.1 * PI, 4.1 * PI]).set_stroke(BLUE, sW)
+        self.play(Create(cosCurve))
+        extremaPoints = np.linspace(-4 * PI, 4 * PI, 9).tolist()
+        # print(extremaPoints)
+        dots = [Dot(axes.c2p(i, cosCurveFunc(i), 0), radius=DEFAULT_DOT_RADIUS / 1.5, color= YELLOW) for i in extremaPoints]
+        posEx = dots[4].copy()
+        negEx = dots[5].copy()
+
+        self.play(*[Create(i) for i in dots])
+        self.add(posEx, negEx)
+        self.play(FadeOut(*dots))     
+
+
+        
+        Dx = .00001
+        x = ValueTracker(-.5 * PI)
+        def getTangentPoints(func):
+            x1 = x.get_value()
+            p1 = axes.c2p(x1, func(x1), 0)
+            p2 = axes.c2p(x1 + Dx, func(x1 + Dx), 0)
+            return p1, p2
+
+        def getTangent(func):
+            p1, p2 = getTangentPoints(func)
+            line = Line(p1, p2).set_length(3.5).set_color((RED,GRAY, BLUE)).set_stroke(width = sW)
+            return line
+
+        tangentDot = Dot(axes.c2p(*[x.get_value(), cosCurveFunc(x.get_value()), 0]), DEFAULT_DOT_RADIUS / 1.5).set_color(ORANGE).set_z_index(2)
+        self.play(FadeIn(tangentDot))
+        tangentDot.add_updater(lambda _: tangentDot.move_to(axes.c2p(*[x.get_value(), cosCurveFunc(x.get_value()), 0])))
+        tangentLine = getTangent(cosCurveFunc)
+        tangentLine.add_updater(lambda _ : tangentLine.become(getTangent(cosCurveFunc)))
+
+        
+
+        self.play(Create(tangentLine))
+
+        
+        def getSlope(func):
+            x1 = x.get_value()
+            p1 = [x1, func(x1), 0]
+            p2 = [x1 + Dx, func(x1 + Dx), 0]
+            slope = (p2[1] - p1[1]) / Dx
+            return [x1, slope, 0]
+        
+        lastPoint = getSlope(cosCurveFunc)
+        lines = VGroup()
+        def plotSlope(_):
+            nonlocal lastPoint
+            sl = getSlope(cosCurveFunc)
+            line = Line(axes.c2p(*lastPoint), axes.c2p(*sl)).set_stroke(color = tangentDot.color, width=sW)
+            lastPoint = sl
+            lines.add(line)
+            
+        lines.add_updater(plotSlope)        
+        self.add(lines)
+
+
+
+        self.play(x.animate.set_value(0))
+        # lines.clear_updaters()
+
+        self.wait()
+        self.play(x.animate.set_value(.5 * PI))
+        self.wait(1)
+        self.play(x.animate.set_value(PI))
+        self.wait(1)
+        self.play(x.animate.set_value(1.5 * PI))
+
+   
+        pass
+
+    def graph2(self):
+        
+        sW = DEFAULT_STROKE_WIDTH / 1.5
+
+        axis_config = {
+            "tick_size" : .05,
+            "include_numbers" : False,
+
+        }
+        axes = Axes(
+            x_range=[-5, 5, 1],
+            y_range =[-10, 10, 2],
+            x_axis_config=axis_config,
+            y_axis_config=axis_config,
+            y_length=config.frame_height - .5
+
+        ).set_color_by_gradient(RED,PURPLE,GRAY).scale(.9).set_stroke(width = sW)
+        def curveFunc(x):
+            return x ** 7 - 6 * x ** 5 + 9 * x ** 3 - 4 * x
+
+        self.play(Create(axes))
+        curve = axes.plot(curveFunc).set_stroke(YELLOW, sW)
+        self.play(Create(curve), run_time = 2)
+
+        
+        Dx = .00001
+        x = ValueTracker(-2)
+        def getTangentPoints(func):
+            x1 = x.get_value()
+            p1 = axes.c2p(x1, func(x1), 0)
+            p2 = axes.c2p(x1 + Dx, func(x1 + Dx), 0)
+            return p1, p2
+
+        def getTangent(func):
+            p1, p2 = getTangentPoints(func)
+            line = Line(p1, p2).set_length(3.5).set_color(WHITE).set_stroke(width = sW)
+            return line
+
+        tangentDot = Dot(axes.c2p(*[x.get_value(), curveFunc(x.get_value()), 0]), DEFAULT_DOT_RADIUS / 1.5).set_color(WHITE).set_z_index(2)
+        self.play(FadeIn(tangentDot))
+        tangentDot.add_updater(lambda _: tangentDot.move_to(axes.c2p(*[x.get_value(), curveFunc(x.get_value()), 0])))
+        tangentLine = getTangent(curveFunc)
+        tangentLine.add_updater(lambda _ : tangentLine.become(getTangent(curveFunc)))
+
+        
+
+        self.play(Create(tangentLine))
+
+        
+        def getSlope(func):
+            x1 = x.get_value()
+            p1 = [x1, func(x1), 0]
+            p2 = [x1 + Dx, func(x1 + Dx), 0]
+            slope = (p2[1] - p1[1]) / Dx
+            return [x1, slope, 0]
+        
+        lastPoint = getSlope(curveFunc)
+        lines = VGroup()
+        def plotSlope(_):
+            nonlocal lastPoint
+            sl = getSlope(curveFunc)
+            line = Line(axes.c2p(*lastPoint), axes.c2p(*sl)).set_stroke(color = tangentDot.color, width=sW)
+            lastPoint = sl
+            lines.add(line)
+            
+        lines.add_updater(plotSlope)        
+        self.add(lines)
+
+        self.play(x.animate.set_value(2), run_time = 10)
+
+    
+
+    def construct(self):
+        # self.incrDecr()
+        # self.turningPoint()
+        self.graph2()
+        pass
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Math Animation Using Manim
+This is a repository of the animation i did for 10 minute school 
+
+## Area of cirle
+link of video : https://fb.watch/cAKCKkHoKd
+## Locus
+link of video : https://fb.watch/cAKzO0uZm0
+## Maxima Minima
+link of video : https://fb.watch/cAKFLvlFXr  
+only some portion of it
+1. animation of the left for first 20s
+2. animation from 1:33 to 1:48
+3. animation from 2:04 to 2:15
